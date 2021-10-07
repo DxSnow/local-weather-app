2fd13b8a94eed18706643102e85e433482697aa6
DxSnow <62160409+DxSnow@users.noreply.github.com>
2021-10-06 20:08:45 -0700

    wrote GetCurrentWeather function in service

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IcwFilter } from './icw-filter';
+import { environment } from 'src/environments/environment';
+import { ICurrentWeather } from './icurrent-weather';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+
+  }
+// do not write below function in constructor because you only want it to run when it is called. Not as soon as it is constructed.
+  getCurrentWeather(city: string, country: string){
+    return this.http.get<IcwFilter>(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.openweatherID}`)
+
+  }
+
+
+
+
 }
